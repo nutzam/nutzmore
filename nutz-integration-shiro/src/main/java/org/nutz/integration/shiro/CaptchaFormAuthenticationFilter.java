265cc4318958ca06646ba63b39759fbca24889d8
wendal <wendal1985@gmail.com>
2015-07-19 22:29:51 +0800

    change: 单独检查验证码了,这样验证码出错和用户名/密码错误的就很方便的分开了

--- nutz-integration-shiro/src/main/java/org/nutz/integration/shiro/CaptchaFormAuthenticationFilter.java
+++ nutz-integration-shiro/src/main/java/org/nutz/integration/shiro/CaptchaFormAuthenticationFilter.java
@@ -6,71 +6,95 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.apache.shiro.web.util.WebUtils;
+import org.nutz.lang.Strings;
 import org.nutz.lang.util.NutMap;
 import org.nutz.mvc.ActionContext;
 import org.nutz.mvc.ActionFilter;
 import org.nutz.mvc.View;
 
 /**
- * @author 科技㊣²º¹³
- * 2014年2月3日 下午4:48:45
- * http://www.rekoe.com
- * QQ:5382211
+ * @author 科技㊣²º¹³ 2014年2月3日 下午4:48:45 http://www.rekoe.com QQ:5382211
  * @author wendal<wendal1985@gmail.com>
  */
 public class CaptchaFormAuthenticationFilter extends FormAuthenticationFilter implements ActionFilter {
-	
-	private String captchaParam = NutShiro.DEFAULT_CAPTCHA_PARAM;
 
-	public String getCaptchaParam() {
-		return captchaParam;
-	}
+    private String captchaParam = NutShiro.DEFAULT_CAPTCHA_PARAM;
 
-	protected String getCaptcha(ServletRequest request) {
-		return WebUtils.getCleanParam(request, getCaptchaParam());
-	}
+    public String getCaptchaParam() {
+        return captchaParam;
+    }
 
-	public AuthenticationToken createToken(ServletRequest request, ServletResponse response) {
-		String username = getUsername(request);
-		String password = getPassword(request);
-		String captcha = getCaptcha(request);
-		boolean rememberMe = isRememberMe(request);
-		String host = getHost(request);
-		return new CaptchaUsernamePasswordToken(username, password, rememberMe, host, captcha);
-	}
-	
-	@Override
-	protected boolean onLoginFailure(AuthenticationToken token,
-			AuthenticationException e, ServletRequest req,
-			ServletResponse resp) {
-		if (NutShiro.isAjax(req)) {
-			NutMap re = new NutMap().setv("ok", false).setv("msg", e.getMessage());
-			NutShiro.rendAjaxResp(req, resp, re);
-			return false;
-		}
-		return super.onLoginFailure(token, e, req, resp);
-	}
-	
-	@Override
-	protected boolean onLoginSuccess(AuthenticationToken token,
-			Subject subject, ServletRequest req, ServletResponse resp)
-			throws Exception {
-		subject.getSession().setAttribute(NutShiro.SessionKey, subject.getPrincipal());
-		if (NutShiro.isAjax(req)) {
-			NutShiro.rendAjaxResp(req, resp, new NutMap().setv("ok", true));
-			return false;
-		}
-		return super.onLoginSuccess(token, subject, req, resp);
-	}
+    protected String getCaptcha(ServletRequest request) {
+        return WebUtils.getCleanParam(request, getCaptchaParam());
+    }
 
-	@Override
-	public View match(ActionContext ac) {
-		HttpServletRequest request = ac.getRequest();
-		AuthenticationToken authenticationToken = createToken(request, ac.getResponse());
-		request.setAttribute("loginToken", authenticationToken);
-		return null;
-	}
+    protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
+        String captcha = getCaptcha(request);
+        if (Strings.isBlank(captcha)) {
+            return onCaptchaError(request, response);
+        }
+        Subject subject = getSubject(request, response);
+        Session session = subject.getSession(false);
+        if (session == null) {
+            return onCaptchaError(request, response);
+        }
+        Object _expected = session.getAttribute(captchaParam);
+        if (_expected == null)
+            _expected = session.getAttribute("nutz_captcha");
+        if (_expected == null)
+            return onCaptchaError(request, response);
+        if (!captcha.equalsIgnoreCase(String.valueOf(_expected)))
+            return onCaptchaError(request, response);
+        String username = getUsername(request);
+        String password = getPassword(request);
+        boolean rememberMe = isRememberMe(request);
+        String host = getHost(request);
+        AuthenticationToken token = new CaptchaUsernamePasswordToken(username, password, rememberMe, host, captcha);
+        try {
+            subject.login(token);
+            return onLoginSuccess(token, subject, request, response);
+        }
+        catch (AuthenticationException e) {
+            return onLoginFailure(token, e, request, response);
+        }
+    }
+
+    protected boolean onCaptchaError(ServletRequest req, ServletResponse resp) {
+        if (NutShiro.isAjax(req)) {
+            NutMap re = new NutMap().setv("ok", false).setv("msg", "验证码错误");
+            NutShiro.rendAjaxResp(req, resp, re);
+            return false;
+        } else {
+            return super.onLoginFailure(null, null, req, resp);
+        }
+    }
+
+    protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest req, ServletResponse resp) {
+        if (NutShiro.isAjax(req)) {
+            NutMap re = new NutMap().setv("ok", false).setv("msg", "登陆失败");
+            NutShiro.rendAjaxResp(req, resp, re);
+            return false;
+        }
+        return super.onLoginFailure(token, e, req, resp);
+    }
+
+    protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest req, ServletResponse resp) throws Exception {
+        subject.getSession().setAttribute(NutShiro.SessionKey, subject.getPrincipal());
+        if (NutShiro.isAjax(req)) {
+            NutShiro.rendAjaxResp(req, resp, new NutMap().setv("ok", true));
+            return false;
+        }
+        return super.onLoginSuccess(token, subject, req, resp);
+    }
+
+    public View match(ActionContext ac) {
+        HttpServletRequest request = ac.getRequest();
+        AuthenticationToken authenticationToken = createToken(request, ac.getResponse());
+        request.setAttribute("loginToken", authenticationToken);
+        return null;
+    }
 }
