65b459f434a71cb54366f31de1b2568be932cc0f
wendal <wendal1985@gmail.com>
2015-07-19 21:43:23 +0800

    change: ValidationProcessor更高效一些

--- nutz-plugins-validation/src/main/java/org/nutz/plugins/validation/ValidationProcessor.java
+++ nutz-plugins-validation/src/main/java/org/nutz/plugins/validation/ValidationProcessor.java
@@ -1,6 +1,8 @@
 package org.nutz.plugins.validation;
 
 import org.nutz.mvc.ActionContext;
+import org.nutz.mvc.ActionInfo;
+import org.nutz.mvc.NutConfig;
 import org.nutz.mvc.impl.processor.AbstractProcessor;
 import org.nutz.plugins.validation.annotation.AnnotationValidation;
 
@@ -10,20 +12,28 @@ import org.nutz.plugins.validation.annotation.AnnotationValidation;
  * 要求 action 参数中必须有一个 Errors 类型的参数（不需要使用 Param 声明）。当验证完成后会向这个参数赋值
  * 
  * @author QinerG(QinerG@gmail.com)
+ * @author wendal(wendal1985@gmail.com)
  */
 public class ValidationProcessor extends AbstractProcessor {
 	
 	private static AnnotationValidation av = new AnnotationValidation();
+	
+	protected boolean hasErrorArg;
+	
+	public void init(NutConfig config, ActionInfo ai) throws Throwable {
+	    for (Class<?> klass : ai.getMethod().getParameterTypes()) {
+            if (klass.isAssignableFrom(Errors.class))
+                hasErrorArg = true;
+        }
+	}
 
 	public void process(ActionContext ac) throws Throwable {
-		Errors es = ValidationUtils.checkArgs(ac.getMethod()
-				.getParameterTypes(), ac.getMethodArgs());
-		// 参数中没找到 Errors 类型的参数，忽略处理，继续其它动作链处理
-		if (null != es) {
-			for (Object obj : ac.getMethodArgs()) {
-				av.validate(obj, es);
-			}
-		}
+	    if (hasErrorArg) {
+	        Errors es = new Errors();
+	        for (Object obj : ac.getMethodArgs()) {
+                av.validate(obj, es);
+            }
+	    }
 		doNext(ac);
 	}
 }
