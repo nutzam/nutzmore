6087b195137fd9de6695f9f748dade5d91db56bf
rekoe <koukou890@qq.com>
2015-07-21 05:44:49 +0800

    整理插件发布结构

--- nutz-plugins-protobuf/conf/ioc/ioc.js
+++
@@ -1,8 +0,0 @@
-var ioc = {
-	protobufAdaptor : {
-		type : "org.nutz.plugins.protobuf.mvc.adaptor.ProtobufAdaptor"
-	},
-	jprotobufAdaptor : {
-		type : "org.nutz.plugins.protobuf.mvc.adaptor.JProtobufAdaptor"
-	}
-};
