fa201609957b1947f9103101694bb70cb81fce09
wendal <wendal1985@gmail.com>
2015-09-16 11:31:42 +0800

    add: jetbrick-template版的SqlTpl实现

---
+++ nutz-plugins-sqltpl/src/main/java/org/nutz/plugins/sqltpl/impl/jetbrick/JetbrickSqlTpl.java
@@ -0,0 +1,37 @@
+package org.nutz.plugins.sqltpl.impl.jetbrick;
+
+import java.io.StringWriter;
+
+import org.nutz.dao.sql.Sql;
+import org.nutz.plugins.sqltpl.VarSetMap;
+
+import jetbrick.template.JetEngine;
+import jetbrick.template.JetTemplate;
+
+/**
+ * jetbrick-template实现的SqlTpl
+ * @author wendal(wendal1985@gmail.com)
+ *
+ */
+public class JetbrickSqlTpl {
+
+    protected static JetEngine engine;
+    
+    public static Sql c(Sql sql) {
+        JetTemplate template = engine().createTemplate(sql.getSourceSql());
+        StringWriter sw = new StringWriter();
+        template.render(VarSetMap.asCtx(sql), sw);
+        sql.setSourceSql(sw.toString());
+        return sql;
+    }
+    
+    public static JetEngine engine() {
+        if (engine == null)
+            engine = JetEngine.create();
+        return engine;
+    }
+    
+    public static void setEngine(JetEngine engine) {
+        JetbrickSqlTpl.engine = engine;
+    }
+}
