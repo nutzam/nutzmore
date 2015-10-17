899f0d6349c2b067805edb66de4d35bc3e407e53
kouxian <koukou890@qq.com>
2015-10-17 20:39:23 +0800

    add demo

--- nutz-integration-hessian/src/main/java/org/nutz/hessian/mvc/adaptor/HessianAdaptor.java
+++ nutz-integration-hessian/src/main/java/org/nutz/hessian/mvc/adaptor/HessianAdaptor.java
@@ -2,18 +2,49 @@ package org.nutz.hessian.mvc.adaptor;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.nutz.hessian.mvc.adaptor.HessianAdaptor;
 import org.nutz.lang.Lang;
 import org.nutz.mvc.adaptor.PairAdaptor;
+import org.nutz.mvc.annotation.AdaptBy;
+import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Ok;
 
 import com.caucho.hessian.io.SerializerFactory;
 import com.caucho.hessian.server.HessianSkeleton;
 import com.caucho.services.server.ServiceContext;
+import com.shanggame.service.HessianClient;
+import com.shanggame.service.SayHelloWorld;
 
+/**
+ * @author koukou890@qq.com
+ * 
+ * demo:
+ * server<br /> 
+ * @AdaptBy(type = HessianAdaptor.class, args = { "com.shanggame.module.SayHelloWorld", "com.shanggame.module.SayHelloWorldImpl" })
+	@At
+	@Ok("void")
+	public void t() {
+
+	}
+ * 
+ * client<br /> 
+ * public static void main(String[] args) {
+		HessianClient c = new HessianClient();
+		try {
+			Object obj = c.factory.create(SayHelloWorld.class, "http://localhost:8080/t");
+			System.out.println(((SayHelloWorld)obj).hi("abc"));
+		} catch (MalformedURLException e) {
+			log.error(e);
+		}
+	}
+ *
+ */
 public class HessianAdaptor extends PairAdaptor {
 
 	private HessianSkeleton _homeSkeleton;
