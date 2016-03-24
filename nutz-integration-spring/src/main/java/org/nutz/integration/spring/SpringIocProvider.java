fdd80916367849536dbbfeb543269cdaa6051e76
王贵源 <kerbores@zhcs.club>
2016-03-24 11:55:37 +0800

    版本统一至 1.r.55-SNAPSHOT

--- nutz-integration-spring/src/main/java/org/nutz/integration/spring/SpringIocProvider.java
+++ nutz-integration-spring/src/main/java/org/nutz/integration/spring/SpringIocProvider.java
@@ -23,45 +23,72 @@ import org.springframework.web.context.WebApplicationContext;
  */
 public class SpringIocProvider implements IocProvider, Ioc {
 
-    protected ApplicationContext applicationContext;
+	protected ApplicationContext applicationContext;
 
-    public Ioc create(NutConfig config, String[] args) {
-        if (config == null || Lang.length(args) > 0)
-            applicationContext = new ClassPathXmlApplicationContext(args);
-        else
-            applicationContext = (ApplicationContext) config.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
-        return this;
-    }
+	@Override
+	public Ioc create(NutConfig config, String[] args) {
+		if (config == null || Lang.length(args) > 0)
+			applicationContext = new ClassPathXmlApplicationContext(args);
+		else
+			applicationContext = (ApplicationContext) config.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+		return this;
+	}
 
-    public void depose() {
-        if (applicationContext != null) {
-            applicationContext.publishEvent(new ContextClosedEvent(applicationContext));
-            applicationContext = null;
-        }
-    }
+	@Override
+	public void depose() {
+		if (applicationContext != null) {
+			applicationContext.publishEvent(new ContextClosedEvent(applicationContext));
+			applicationContext = null;
+		}
+	}
 
-    public <T> T get(Class<T> type, String name) {
-        return (T) applicationContext.getBean(name, type);
-    }
+	@Override
+	public <T> T get(Class<T> type, String name) {
+		return applicationContext.getBean(name, type);
+	}
 
-    public String[] getNames() {
-        return applicationContext.getBeanDefinitionNames();
-    }
+	@Override
+	public String[] getNames() {
+		return applicationContext.getBeanDefinitionNames();
+	}
 
-    public boolean has(String name) {
-        return applicationContext.containsBean(name);
-    }
+	@Override
+	public boolean has(String name) {
+		return applicationContext.containsBean(name);
+	}
 
-    public void reset() {
-        applicationContext.publishEvent(new ContextRefreshedEvent(applicationContext));
-    }
+	@Override
+	public void reset() {
+		applicationContext.publishEvent(new ContextRefreshedEvent(applicationContext));
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public <T> T get(Class<T> classZ) throws IocException {
-        InjectName injectName = classZ.getAnnotation(InjectName.class);
-        if (injectName != null && !Strings.isBlank(injectName.value()))
-            return (T) applicationContext.getBean(injectName.value());
-        return (T) applicationContext.getBean(applicationContext.getBeanNamesForType(classZ)[0]);
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T get(Class<T> classZ) throws IocException {
+		InjectName injectName = classZ.getAnnotation(InjectName.class);
+		if (injectName != null && !Strings.isBlank(injectName.value()))
+			return (T) applicationContext.getBean(injectName.value());
+		return (T) applicationContext.getBean(applicationContext.getBeanNamesForType(classZ)[0]);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.nutz.ioc.Ioc#getNamesByType(java.lang.Class)
+	 */
+	@Override
+	public String[] getNamesByType(Class<?> klass) {
+
+		return applicationContext.getBeanDefinitionNames();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.nutz.ioc.Ioc#getByType(java.lang.Class)
+	 */
+	@Override
+	public <K> K getByType(Class<K> klass) {
+		return applicationContext.getBean(klass);
+	}
 }
--- nutz-integration-struts2/src/main/java/org/nutz/integration/struts2/NutObjectFactory.java
+++ nutz-integration-struts2/src/main/java/org/nutz/integration/struts2/NutObjectFactory.java
@@ -12,48 +12,54 @@ import org.nutz.mvc.NutMvcListener;
 import com.opensymphony.xwork2.ObjectFactory;
 
 /**
- * 需要2个配置, 首先在struts.properties加入 struts.objectFactory=org.nutz.integration.struts2.NutObjectFactory<p/>
- * 第二步有2种选择, 推荐在web.xml声明org.nutz.mvc.NutMvcListener, 或者直接 struts.objectFactory.iocArgs=*js,ioc/,*anno,net.wendal.nutzbook
+ * 需要2个配置, 首先在struts.properties加入
+ * struts.objectFactory=org.nutz.integration.struts2.NutObjectFactory
+ * <p/>
+ * 第二步有2种选择, 推荐在web.xml声明org.nutz.mvc.NutMvcListener, 或者直接
+ * struts.objectFactory.iocArgs=*js,ioc/,*anno,net.wendal.nutzbook
+ * 
  * @author wendal(wendal1985@gmail.com)
  * @see https://struts.apache.org/docs/objectfactory.html
  */
 public class NutObjectFactory extends ObjectFactory {
-    
-    private static final long serialVersionUID = 8509262184575823946L;
 
-    public NutObjectFactory() {}
-    
-    protected Ioc ioc;
-    
-    protected boolean selfIoc;
-    
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    @Override
-    public Object buildBean(Class clazz, Map<String, Object> extraContext) throws Exception {
-        if (clazz.getAnnotation(IocBean.class) != null) {
-            if (ioc == null) {
-                ioc = Mvcs.getIoc();
-                if (ioc == null)
-                    ioc = Mvcs.ctx().getDefaultIoc();
-                if (ioc == null)
-                    ioc = NutMvcListener.ioc();
-            }
-            return ioc.get(clazz);
-        }
-        return super.buildBean(clazz, extraContext);
-    }
-    
-    public void setIoc(Ioc ioc) {
-        this.ioc = ioc;
-    }
-    
-    public void setIocArgs(String args) throws ClassNotFoundException {
-        this.ioc = new NutIoc(new ComboIocLoader(args.trim().split(",")));
-        selfIoc = true;
-    }
-    
-    public boolean isNoArgConstructorRequired() {
-        return false;
-    }
-    
+	private static final long serialVersionUID = 8509262184575823946L;
+
+	public NutObjectFactory() {
+	}
+
+	protected Ioc ioc;
+
+	protected boolean selfIoc;
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public Object buildBean(Class clazz, Map<String, Object> extraContext) throws Exception {
+		if (clazz.getAnnotation(IocBean.class) != null) {
+			if (ioc == null) {
+				ioc = Mvcs.getIoc();
+				if (ioc == null)
+					ioc = Mvcs.ctx().getDefaultIoc();
+				if (ioc == null)
+					ioc = NutMvcListener.ioc();
+			}
+			return ioc.get(clazz);
+		}
+		return super.buildBean(clazz, extraContext);
+	}
+
+	public void setIoc(Ioc ioc) {
+		this.ioc = ioc;
+	}
+
+	public void setIocArgs(String args) throws ClassNotFoundException {
+		this.ioc = new NutIoc(new ComboIocLoader(args.trim().split(",")));
+		selfIoc = true;
+	}
+
+	@Override
+	public boolean isNoArgConstructorRequired() {
+		return false;
+	}
+
 }
