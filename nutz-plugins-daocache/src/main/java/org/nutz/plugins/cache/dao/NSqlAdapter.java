ca1dfb7582d494295965cce4247f85d276e59f16
wendal chen <wendal1985@gmail.com>
2016-06-20 15:43:03 +0800

    update: oracle下会cast错误

--- nutz-plugins-daocache/src/main/java/org/nutz/plugins/cache/dao/NSqlAdapter.java
+++ nutz-plugins-daocache/src/main/java/org/nutz/plugins/cache/dao/NSqlAdapter.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.alibaba.druid.sql.ast.statement.SQLExprTableSource;
-import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlASTVisitorAdapter;
+import com.alibaba.druid.sql.visitor.SQLASTVisitorAdapter;
 
 /**
  * 通过Druid的遍历器获取sql所操作的数据库表
  * @author wendal(wendal1985@gmail.com)
  *
  */
-public class NSqlAdapter extends MySqlASTVisitorAdapter {
+public class NSqlAdapter extends SQLASTVisitorAdapter {
 
     public List<String> tableNames = new ArrayList<String>();
 
