2459f8fbb1d71e186562a0a8e51e21f7998eab0a
wendal chen <wendal1985@gmail.com>
2016-07-22 12:28:27 +0800

    add: nutz-plugins-jedis
    Signed-off-by: wendal chen <wendal1985@gmail.com>

---
+++ nutz-plugins-jedis/src/main/java/org/nutz/plugin/jedis/RedisInterceptor.java
@@ -0,0 +1,34 @@
+package org.nutz.plugin.jedis;
+
+import org.nutz.aop.InterceptorChain;
+import org.nutz.aop.MethodInterceptor;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+@IocBean(name="redis")
+public class RedisInterceptor implements MethodInterceptor {
+
+	@Inject JedisPool jedisPool;
+	
+	static ThreadLocal<Jedis> TL = new ThreadLocal<Jedis>();
+	
+	public void filter(InterceptorChain chain) throws Throwable {
+		if (TL.get() != null) {
+			chain.doChain();
+			return;
+		}
+		try (Jedis jedis = jedisPool.getResource()) {
+			TL.set(jedis);
+			chain.doChain();
+		} finally{
+			TL.remove();
+		}
+	}
+
+	public static Jedis jedis() {
+		return TL.get();
+	}
+}
