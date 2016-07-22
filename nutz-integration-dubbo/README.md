e2bb45af4ffc53540ab7dab68ada47ef4cb35bf1
wendal chen <wendal1985@gmail.com>
2016-07-22 17:46:54 +0800

    add: nutz-integration-dubbo 集成dubbo的beta实现
    Signed-off-by: wendal chen <wendal1985@gmail.com>

---
+++ nutz-integration-dubbo/README.md
@@ -0,0 +1,52 @@
+Nutz集成Dubbo的插件
+======================
+
+本插件未严格测试
+-------------------------
+
+* 一个预定义的IocLoader,包含Dubbo启动所需要的bean的定义
+* 一个Dubbo注解扫描器
+
+使用方法
+-------------------------
+
+* 添加dubbo依赖及本插件的依赖
+* 在MainModule的IocBy中直接 *dubbo
+* 在MainSetup的init方法中获取
+
+示例IocBy配置
+----------------------------------------------
+
+```java
+	@IocBy(type=ComboIocProvider.class, args={"*js", "ioc/",
+										   "*anno", "net.wendal.nutzbook",
+										   "*dubbo"})
+```
+								   
+在MainSetup的init方法中加载
+-----------------------------------------------
+
+```java
+ioc.get(DubboAnnotationLoader.class, "dubbo_anno");
+// 该操作会触发生产者和消费者的初始化
+```
+
+配置项
+-----------------------------------------------
+
+配置项通过名为conf的IocBean获取, 通常是PropertiesProxy类的实例
+
+```ini
+dubbo.application.name=nutzbook
+
+dubbo.registry.name=wendal
+dubbo.registry.address=10.20.130.230:9090
+dubbo.registry.username=wendal
+dubbo.registry.password=root
+
+dubbo.protocol.name=dubbo
+dubbo.protocol.port=123456
+dubbo.protocol.threads=256
+
+dubbo.anno.packages=net.wendal.nutzbook
+```
---
+++ nutz-integration-dubbo/src/main/java/org/nutz/integration/dubbo/DubboIocLoader.java
@@ -0,0 +1,44 @@
+package org.nutz.integration.dubbo;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import org.nutz.ioc.loader.json.JsonLoader;
+import org.nutz.json.Json;
+import org.nutz.lang.Lang;
+import org.nutz.lang.Streams;
+
+public class DubboIocLoader extends JsonLoader {
+    
+    public DubboIocLoader() {
+        _load("dubbo.js", "conf");
+    }
+
+    public DubboIocLoader(String... args) {
+        String confName = args.length > 0 ? args[0] : "conf";
+        _load("dubbo.js", confName);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void _load(String path, String confName) {
+        InputStream ins = getClass().getClassLoader().getResourceAsStream("ioc/" + path);
+        if (ins == null)
+            ins = getClass().getResourceAsStream(path);
+        if (ins == null)
+            return;
+        try {
+            String s = Lang.readAll(new InputStreamReader(ins));
+            s = s.replace("confName", confName);
+            Map<String, Map<String, Object>> map = (Map<String, Map<String, Object>>) Json.fromJson(s);
+            if (null != map && map.size() > 0)
+                getMap().putAll(map);
+        } catch (Exception e) {
+            if (e instanceof RuntimeException)
+                throw (RuntimeException) e;
+            throw new RuntimeException("load fail , path=" + path, e);
+        } finally {
+            Streams.safeClose(ins);
+        }
+    }
+}
