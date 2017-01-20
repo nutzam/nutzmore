136ca9f3d31d1a1abc2a1efb2d0866cf8a19d5ca
wendal chen <wendal1985@gmail.com>
2017-01-20 18:48:37 +0800

    remove: shiro的缓存实现放在新的插件目录

--- nutz-integration-shiro/src/main/java/org/nutz/integration/shiro/cache/RedisCacheManager.java
+++ nutz-integration-shiro/src/main/java/org/nutz/integration/shiro/cache/RedisCacheManager.java
@@ -1,26 +1,11 @@
 package org.nutz.integration.shiro.cache;
 
-import org.apache.shiro.cache.Cache;
-import org.apache.shiro.cache.CacheException;
-import org.apache.shiro.cache.CacheManager;
-
 /**
- * 基于 hset的缓存实现
+ * 
  * @author wendal
  *
  */
-public class RedisCacheManager implements CacheManager {
+@Deprecated
+public class RedisCacheManager extends org.nutz.plugins.cache.impl.redis.RedisCacheManager {
     
-    protected String mode;
-
-    public <K, V> Cache<K, V> getCache(String name) throws CacheException {
-        if (mode == null || mode.equals("hset"))
-            return (Cache<K, V>) new RedisCache<K, V>().setName(name);
-        return (Cache<K, V>) new RedisCache2<K, V>().setName(name);
-    }
-    
-    public void setMode(String mode) {
-        this.mode = mode;
-    }
-
 }
