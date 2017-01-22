11e5286c9b3ab7a0badccc7ebc70127b3acebdfb
wendal chen <wendal1985@gmail.com>
2017-01-22 11:06:09 +0800

    add: 为JedisAgent添加一个spring factory bean

---
+++ nutz-integration-jedis/src/main/java/org/nutz/integration/jedis/JedisAgentFactoryBean.java
@@ -0,0 +1,42 @@
+package org.nutz.integration.jedis;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPool;
+
+public class JedisAgentFactoryBean implements FactoryBean<JedisAgent>, BeanFactoryPostProcessor {
+    
+    protected String mode;
+    
+    protected JedisAgent jedisAgent;
+
+    public JedisAgent getObject() throws Exception {
+        return jedisAgent;
+    }
+
+    public Class<?> getObjectType() {
+        return JedisAgent.class;
+    }
+
+    public boolean isSingleton() {
+        return true;
+    }
+    
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+            throws BeansException {
+        if ("cluster".equals(mode)) {
+            jedisAgent = new JedisAgent(beanFactory.getBean(JedisCluster.class));
+        } else {
+            jedisAgent = new JedisAgent(beanFactory.getBean(JedisPool.class));
+        }
+    }
+
+}
