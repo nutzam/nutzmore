71270f56c69879973b20c9275ca09680897bb731
Kerbores <Kerbores@gmail.com>
2017-02-25 22:59:15 +0800

    add:apidoc demo

---
+++ nutz-plugins-apidoc/demo/src/main/java/org/nutz/apidoc/demo/module/MainModule.java
@@ -0,0 +1,37 @@
+package org.nutz.apidoc.demo.module;
+
+import org.nutz.lang.util.NutMap;
+import org.nutz.mvc.adaptor.JsonAdaptor;
+import org.nutz.mvc.annotation.AdaptBy;
+import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.IocBy;
+import org.nutz.mvc.annotation.Modules;
+import org.nutz.mvc.annotation.Ok;
+import org.nutz.mvc.annotation.UrlMappingBy;
+import org.nutz.mvc.ioc.provider.ComboIocProvider;
+import org.nutz.plugins.apidoc.ApidocUrlMapping;
+import org.nutz.plugins.apidoc.annotation.Api;
+import org.nutz.plugins.apidoc.annotation.ApiMatchMode;
+import org.nutz.plugins.apidoc.annotation.Document;
+
+@Modules
+@IocBy(type = ComboIocProvider.class, args = { "*anno", "org.nutz.apidoc", "*async" })
+@Ok("json")
+@UrlMappingBy(ApidocUrlMapping.class)
+@Document(name="测试",description="scadcew",author="Kerbores",email="kerbores@gmail.com",homePage="http://www.baidu.com")
+@Api(author="kkk",name="KKK",match=ApiMatchMode.ALL)
+public class MainModule {
+
+	@At
+	@Api(name = "index",description="我是描述字段")
+	public NutMap index() {
+		return NutMap.NEW().addv("msg", "Hello Nutz!");
+	}
+
+	@At
+	@AdaptBy(type = JsonAdaptor.class)
+	public NutMap name(NutMap data) {
+		return NutMap.NEW().addv("msg", "Hello Nutz!");
+	}
+
+}
