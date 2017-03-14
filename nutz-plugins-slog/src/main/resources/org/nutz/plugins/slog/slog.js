488d258098d242d89fd71789b2b6f2468e99a04e
wendal chen <wendal1985@gmail.com>
2017-03-14 13:40:52 +0800

    fix: slog.js里面的slogService定义被删了

--- nutz-plugins-slog/src/main/resources/org/nutz/plugins/slog/slog.js
+++ nutz-plugins-slog/src/main/resources/org/nutz/plugins/slog/slog.js
@@ -1,5 +1,11 @@
 var ioc = {
 	$aop_slog : {
 		type : "org.nutz.plugins.slog.aop.SlogAopConfigration"
-	}
+	},
+    slogService : {		
+    	type : "org.nutz.plugins.slog.service.SlogService",		
+    	fields : {		
+    		dao : {refer:"dao"}		
+    	}		
+    }
 }
