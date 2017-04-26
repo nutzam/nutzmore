8502020f4be33c177d1d2d129edbf35803b0d6e3
wendal chen <wendal1985@gmail.com>
2017-04-26 11:58:26 +0800

    add: grpc集成的beta实现

---
+++ nutz-integration-grpc/src/test/java/org/nutz/integration/grpc/GrpcProtoMakerTest.java
@@ -0,0 +1,23 @@
+package org.nutz.integration.grpc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GrpcProtoMakerTest {
+
+    @Test
+    public void testMake() {
+        GrpcProtoMaker maker = new GrpcProtoMaker();
+        maker.klass = SimpleGrpcService.class;
+        maker.make();
+        System.out.println("-====================");
+        System.out.println(maker.proto);
+        System.out.println("-====================");
+        System.out.println(maker.serverProxy);
+        System.out.println("-====================");
+        System.out.println(maker.clientProxy);
+        System.out.println("-====================");
+    }
+
+}
