4a921901eb93ab01b8735a7a0451f3455ea12bf2
wendal chen <wendal1985@gmail.com>
2017-05-23 20:21:20 +0800

    add: 支持从activity内部访问nutz ioc的bean

--- nutz-integration-activiti/src/main/java/org/nutz/integration/activiti/ActivitiFactory.java
+++ nutz-integration-activiti/src/main/java/org/nutz/integration/activiti/ActivitiFactory.java
@@ -2,8 +2,9 @@ package org.nutz.integration.activiti;
 
 import javax.sql.DataSource;
 
-import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.engine.impl.scripting.ResolverFactory;
 import org.nutz.ioc.impl.PropertiesProxy;
 import org.nutz.lang.Mirror;
 import org.nutz.log.Log;
@@ -12,8 +13,8 @@ import org.nutz.log.Logs;
 public class ActivitiFactory {
     
     private static final Log log = Logs.get();
-
-    public static ProcessEngine build(DataSource ds, PropertiesProxy conf) {
+    
+    public static ProcessEngineConfiguration build(DataSource ds, PropertiesProxy conf, ResolverFactory rf) {
         StandaloneProcessEngineConfiguration spec = new StandaloneProcessEngineConfiguration();
         spec.setDataSource(ds);
         Mirror<StandaloneProcessEngineConfiguration> mirror = Mirror.me(StandaloneProcessEngineConfiguration.class);
@@ -26,6 +27,8 @@ public class ActivitiFactory {
         }
         if (log.isDebugEnabled())
             log.debug("databaseSchemaUpdate = [" + spec.getDatabaseSchemaUpdate() + "]");
-        return spec.buildProcessEngine();
+        if (rf != null)
+            spec.getResolverFactories().add(rf);
+        return spec;
     }
 }
--- nutz-integration-activiti/src/main/resources/org/nutz/integration/activiti/activiti-engine.js
+++ nutz-integration-activiti/src/main/resources/org/nutz/integration/activiti/activiti-engine.js
@@ -1,8 +1,18 @@
 var ioc = {
-		processEngine : {
+		activitiBeansResolverFactory : {
+			type : "org.nutz.integration.activiti.ActivitiNutIocBeansResolverFactory",
+			fields : {
+				ioc : {refer:"$ioc"}
+			}
+		},
+		processEngineSpec : {
 			factory: "org.nutz.integration.activiti.ActivitiFactory#build",
 			args : [
-				{refer:"dataSource"}, {refer:"conf"}
+				{refer:"dataSource"}, {refer:"conf"}, {refer:"activitiBeansResolverFactory"}
 			]
+		},
+		processEngine : {
+			type : "org.activiti.engine.ProcessEngine",
+			factory : "$processEngineSpec#buildProcessEngine"
 		}
 };
