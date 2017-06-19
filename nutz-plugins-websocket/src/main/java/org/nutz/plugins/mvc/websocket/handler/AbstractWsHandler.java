9581089dab1d9bd02fbe208f9ba0300ddcf707fe
wendal chen <wendal1985@gmail.com>
2017-06-19 12:23:18 +0800

    update: 让websocket插件的SimpleWsHandler具备更多默认方法

--- nutz-plugins-websocket/src/main/java/org/nutz/plugins/mvc/websocket/handler/AbstractWsHandler.java
+++ nutz-plugins-websocket/src/main/java/org/nutz/plugins/mvc/websocket/handler/AbstractWsHandler.java
@@ -1,12 +1,22 @@
 package org.nutz.plugins.mvc.websocket.handler;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
 
+import org.nutz.Nutz;
+import org.nutz.json.Json;
+import org.nutz.json.JsonFormat;
 import org.nutz.lang.Strings;
+import org.nutz.lang.reflect.FastClassFactory;
+import org.nutz.lang.reflect.FastMethod;
+import org.nutz.lang.util.Callback;
+import org.nutz.lang.util.NutMap;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
 import org.nutz.plugins.mvc.websocket.AbstractWsEndpoint;
@@ -25,6 +35,8 @@ public abstract class AbstractWsHandler implements WsHandler {
     protected String prefix;
     protected HttpSession httpSession;
     protected AbstractWsEndpoint endpoint;
+
+    protected Map<String, Callback<NutMap>> actions = new HashMap<>();
 
     public AbstractWsHandler(String prefix) {
         rooms = new HashSet<>();
@@ -53,6 +65,84 @@ public abstract class AbstractWsHandler implements WsHandler {
         for (String room : rooms) {
             left(room);
         }
+    }
+
+
+    public void init() {
+        // 遍历所有public方法
+        for (final Method method : getClass().getMethods()) {
+            Class<?>[] paramTypes = method.getParameterTypes();
+            // 如果仅一个参数,且参数类型NutMap的话, 就判定是WsAction方法咯
+            if (paramTypes.length == 1 && NutMap.class.isAssignableFrom(paramTypes[0])) {
+                // 如果有FastMethod实现的话...
+                if (Nutz.majorVersion() == 1 && Nutz.minorVersion() > 60) {
+                    final FastMethod fm = FastClassFactory.get(method);
+                    actions.put(method.getName(), new Callback<NutMap>() {
+                        public void invoke(NutMap msg) {
+                            try {
+                                fm.invoke(AbstractWsHandler.this, msg);
+                            }
+                            catch (Throwable e) {
+                                onActionError(msg, e);
+                            }
+                        }
+                    });
+                } 
+                // 老版本的话...
+                else {
+                    actions.put(method.getName(), new Callback<NutMap>() {
+                        public void invoke(NutMap msg) {
+                            try {
+                                method.invoke(AbstractWsHandler.this, msg);
+                            }
+                            catch (Throwable e) {
+                                onActionError(msg, e);
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 处理消息, 将其转为NutMap,然后找对应的处理方法.
+     */
+    public void onMessage(String message) {
+        try {
+            NutMap msg = Json.fromJson(NutMap.class, message);
+            String action = msg.getString("action");
+            if (Strings.isBlank(action))
+                return;
+            Callback<NutMap> at = actions.get(action);
+            if (at != null)
+                at.invoke(msg);
+            else
+                defaultAction(msg);
+        }
+        catch (Throwable e) {
+            onActionError(null, e);
+        }
+    }
+    
+    /**
+     * 抛出异常的时候调用之
+     */
+    public void onActionError(NutMap msg, Throwable e) {
+        if (log.isInfoEnabled())
+            log.infof("bad message ? msg=%s",
+                      Json.toJson(msg, JsonFormat.compact().setIgnoreNull(false)),
+                      e);
+    }
+
+    /**
+     * 没有任何action方法对应时,就调用它咯
+     */
+    public void defaultAction(NutMap msg) {
+        if (log.isDebugEnabled())
+            log.debugf("unknown action msg = %s",
+                       Json.toJson(msg, JsonFormat.compact().setIgnoreNull(false)));
     }
 
     public void setRoomProvider(WsRoomProvider roomProvider) {
--- nutz-plugins-websocket/src/main/java/org/nutz/plugins/mvc/websocket/handler/SimpleWsHandler.java
+++ nutz-plugins-websocket/src/main/java/org/nutz/plugins/mvc/websocket/handler/SimpleWsHandler.java
@@ -1,27 +1,15 @@
 package org.nutz.plugins.mvc.websocket.handler;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.websocket.MessageHandler;
+import javax.websocket.Session;
 
-import org.nutz.Nutz;
-import org.nutz.json.Json;
-import org.nutz.json.JsonFormat;
+import org.nutz.lang.Each;
 import org.nutz.lang.Strings;
-import org.nutz.lang.reflect.FastClassFactory;
-import org.nutz.lang.reflect.FastMethod;
-import org.nutz.lang.util.Callback;
 import org.nutz.lang.util.NutMap;
-import org.nutz.log.Log;
-import org.nutz.log.Logs;
 
 public class SimpleWsHandler extends AbstractWsHandler implements MessageHandler.Whole<String> {
-
-    private static final Log log = Logs.get();
-
-    protected Map<String, Callback<NutMap>> actions = new HashMap<>();
+    
+    protected String nickname;
 
     public SimpleWsHandler() {
         this("wsroom:");
@@ -29,53 +17,6 @@ public class SimpleWsHandler extends AbstractWsHandler implements MessageHandler
 
     public SimpleWsHandler(String prefix) {
         super(prefix);
-    }
-
-    public void init() {
-        // 遍历所有public方法
-        for (final Method method : getClass().getMethods()) {
-            Class<?>[] paramTypes = method.getParameterTypes();
-            // 如果仅一个参数,且参数类型NutMap的话, 就判定是WsAction方法咯
-            if (paramTypes.length == 1 && NutMap.class.isAssignableFrom(paramTypes[0])) {
-                // 如果有FastMethod实现的话...
-                if (Nutz.majorVersion() == 1 && Nutz.minorVersion() > 60) {
-                    final FastMethod fm = FastClassFactory.get(method);
-                    actions.put(method.getName(), new Callback<NutMap>() {
-                        public void invoke(NutMap msg) {
-                            try {
-                                fm.invoke(SimpleWsHandler.this, msg);
-                            }
-                            catch (Throwable e) {
-                                onActionError(msg, e);
-                            }
-                        }
-                    });
-                } 
-                // 老版本的话...
-                else {
-                    actions.put(method.getName(), new Callback<NutMap>() {
-                        public void invoke(NutMap msg) {
-                            try {
-                                method.invoke(SimpleWsHandler.this, msg);
-                            }
-                            catch (Throwable e) {
-                                onActionError(msg, e);
-                            }
-                        }
-                    });
-                }
-            }
-        }
-    }
-
-    /**
-     * 抛出异常的时候调用之
-     */
-    public void onActionError(NutMap msg, Throwable e) {
-        if (log.isInfoEnabled())
-            log.infof("bad message ? msg=%s",
-                      Json.toJson(msg, JsonFormat.compact().setIgnoreNull(false)),
-                      e);
     }
 
     /**
@@ -91,33 +32,35 @@ public class SimpleWsHandler extends AbstractWsHandler implements MessageHandler
     public void left(NutMap msg) {
         left(msg.getString("room"));
     }
-
+    
     /**
-     * 没有任何action方法对应时,就调用它咯
+     * 设置昵称
      */
-    public void defaultAction(NutMap msg) {
-        if (log.isDebugEnabled())
-            log.debugf("unknown action msg = %s",
-                       Json.toJson(msg, JsonFormat.compact().setIgnoreNull(false)));
+    public void nickname(NutMap msg) {
+        String nickname = msg.getString("nickname");
+        if (!Strings.isBlank(nickname))
+            this.nickname = nickname;
     }
-
+    
     /**
-     * 处理消息, 将其转为NutMap,然后找对应的处理方法.
+     * 发送消息给房间
      */
-    public void onMessage(String message) {
-        try {
-            NutMap msg = Json.fromJson(NutMap.class, message);
-            String action = msg.getString("action");
-            if (Strings.isBlank(action))
-                return;
-            Callback<NutMap> at = actions.get(action);
-            if (at != null)
-                at.invoke(msg);
-            else
-                defaultAction(msg);
-        }
-        catch (Throwable e) {
-            onActionError(null, e);
-        }
+    public void msg2room(final NutMap msg) {
+        final String room = msg.getString("room");
+        if (room == null)
+            return;
+        endpoint.each(room, new Each<Session>() {
+            public void invoke(int index, Session ele, int length) {
+                if (ele.getId().equals(session.getId()))
+                    return;
+                NutMap resp = new NutMap("action", "msg");
+                resp.setv("room", room);
+                resp.setv("from", session.getId());
+                resp.setv("msg", msg.get("data"));
+                if (nickname != null)
+                    resp.setv("nickname", nickname);
+                endpoint.sendJson(ele.getId(), resp);
+            }
+        });
     }
 }
