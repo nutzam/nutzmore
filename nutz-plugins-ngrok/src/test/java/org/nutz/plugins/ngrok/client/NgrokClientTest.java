3fb4defc2424fa9865360954b47257649623b4e2
kerbores <kerbores@demo.kerbores.com>
2017-08-05 10:25:12 +0800

    clean：never used import

--- nutz-plugins-ngrok/src/test/java/org/nutz/plugins/ngrok/client/NgrokClientTest.java
+++ nutz-plugins-ngrok/src/test/java/org/nutz/plugins/ngrok/client/NgrokClientTest.java
@@ -1,26 +1,25 @@
 package org.nutz.plugins.ngrok.client;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.nutz.lang.Lang;
 
 public class NgrokClientTest {
 
-    @Test
-    public void testStart() {
-        while (true) {
-            final NgrokClient client = new NgrokClient();
-            client.auth_token = "s67r4ilqh6jh3r4v17lggrb93";
-            new Thread() {
-                public void run() {
-                    client.start();
-                    Lang.quiteSleep(1000);
-                    client.stop();
-                };
-            }.start();
-            Lang.quiteSleep(10);
-        }
-    }
+	@Test
+	public void testStart() {
+		while (true) {
+			final NgrokClient client = new NgrokClient();
+			client.auth_token = "s67r4ilqh6jh3r4v17lggrb93";
+			new Thread() {
+				@Override
+				public void run() {
+					client.start();
+					Lang.quiteSleep(1000);
+					client.stop();
+				};
+			}.start();
+			Lang.quiteSleep(10);
+		}
+	}
 
 }
