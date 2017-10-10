0c6418b51253fb449dc23f223fa5bef0c62dfcf4
wendal chen <wendal1985@gmail.com>
2017-10-10 17:08:19 +0800

    fix: NutSqlTpl先调用getParamMatrix然后再调用toPreparedStatement,有可能导致数组越界的异常

--- nutz-plugins-sqltpl/src/main/java/org/nutz/plugins/sqltpl/NutSqlTpl.java
+++ nutz-plugins-sqltpl/src/main/java/org/nutz/plugins/sqltpl/NutSqlTpl.java
@@ -1,18 +1,38 @@
 package org.nutz.plugins.sqltpl;
 
 import org.nutz.dao.impl.sql.NutSql;
+import org.nutz.dao.jdbc.ValueAdaptor;
 
 public abstract class NutSqlTpl extends NutSql {
 
     private static final long serialVersionUID = 1L;
+    
+    protected boolean renderComplete;
 
     public NutSqlTpl(String source) {
         super(source);
     }
+    
+    public ValueAdaptor[] getAdaptors() {
+        checkRender();
+        return super.getAdaptors();
+    }
 
     public String toPreparedStatement() {
-        render();
+        checkRender();
         return super.toPreparedStatement();
+    }
+    
+    public Object[][] getParamMatrix() {
+        checkRender();
+        return super.getParamMatrix();
+    }
+    
+    protected void checkRender() {
+        if (!renderComplete) {
+            render();
+            renderComplete = true;
+        }
     }
     
     protected abstract void render();
