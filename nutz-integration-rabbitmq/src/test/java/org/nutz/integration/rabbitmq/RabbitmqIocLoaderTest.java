ce0a547f4386b0446cca17bbd99f0482249a54c4
ywjno <ywjno.dev@gmail.com>
2017-10-23 16:41:18 +0800

    update: 更新 rabbitmq 插件中 rabbitmq 的版本号

--- nutz-integration-rabbitmq/src/test/java/org/nutz/integration/rabbitmq/RabbitmqIocLoaderTest.java
+++ nutz-integration-rabbitmq/src/test/java/org/nutz/integration/rabbitmq/RabbitmqIocLoaderTest.java
@@ -1,10 +1,14 @@
 package org.nutz.integration.rabbitmq;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,22 +17,18 @@ import org.nutz.ioc.impl.NutIoc;
 import org.nutz.ioc.loader.combo.ComboIocLoader;
 import org.nutz.lang.Lang;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DefaultConsumer;
-import com.rabbitmq.client.Envelope;
-import com.rabbitmq.client.AMQP.BasicProperties;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class RabbitmqIocLoaderTest {
-    
-    Ioc ioc;
-    
+
+    private Ioc ioc;
+
     @Before
     public void before() throws ClassNotFoundException {
         ioc = new NutIoc(new ComboIocLoader("*js", "ioc/", "*anno", "org.nutz.integration.rabbitmq", "*rabbitmq"));
     }
-    
+
     @After
     public void after() {
         if (ioc != null)
@@ -39,19 +39,18 @@ public class RabbitmqIocLoaderTest {
     public void testRabbitmqIocLoader() throws IOException, TimeoutException {
         final String QUEUE_NAME = "hello";
         ConnectionFactory cf = ioc.get(ConnectionFactory.class, "rabbitmq_cf");
-        Connection conn = cf.newConnection();
-        assertNotNull(conn);
-        conn.close();
-        
+        try (Connection conn = cf.newConnection();) {
+            assertNotNull(conn);
+        }
+
         RabbitTestService ts = ioc.get(RabbitTestService.class);
-        
 
         // 开一个发送者, 创建queue.
         Connection sender_conn = cf.newConnection();
         Channel sender_channel = sender_conn.createChannel();
         sender_channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         sender_conn.close();
-        
+
         // 开一个接收者
         final String[] msg = new String[1];
         Connection consumer_conn = cf.newConnection();
@@ -62,13 +61,13 @@ public class RabbitmqIocLoaderTest {
                                        BasicProperties properties,
                                        byte[] body)
                     throws IOException {
-                
+
                 msg[0] = new String(body);
             }
         });
-        
+
         ts.publish(QUEUE_NAME, "wendal".getBytes());
-        
+
         Lang.quiteSleep(1000);
         consumer_channel.close();
         assertEquals("wendal", msg[0]);
