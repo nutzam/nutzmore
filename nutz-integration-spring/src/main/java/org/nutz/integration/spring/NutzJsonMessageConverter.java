a22eaafc0b500363ac79dd530206ab06549f6d3c
kerbores <kerbores@zhcs.club>
2017-11-03 17:49:54 +0800

    fix:json适配泛型问题

--- nutz-integration-spring/src/main/java/org/nutz/integration/spring/NutzJsonMessageConverter.java
+++ nutz-integration-spring/src/main/java/org/nutz/integration/spring/NutzJsonMessageConverter.java
@@ -3,19 +3,17 @@ package org.nutz.integration.spring;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.nio.charset.Charset;
+import java.lang.reflect.Type;
 import java.util.regex.Pattern;
 
 import org.nutz.json.Json;
 import org.nutz.json.JsonFormat;
 import org.nutz.lang.Lang;
 import org.nutz.lang.Strings;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
@@ -23,9 +21,7 @@ import org.springframework.http.converter.HttpMessageNotWritableException;
  * @author Kerbores(kerbores@gamil.com)
  *
  */
-public class NutzJsonMessageConverter extends AbstractHttpMessageConverter<Object> {
-
-	public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+public class NutzJsonMessageConverter extends AbstractGenericHttpMessageConverter<Object> {
 
 	JsonFormat format = JsonFormat.compact();
 
@@ -51,52 +47,29 @@ public class NutzJsonMessageConverter extends AbstractHttpMessageConverter<Objec
 	{
 		setSupportedMediaTypes(Lang.array2list(new MediaType[] { MediaType.APPLICATION_JSON_UTF8 }));
 	}
+	
+	 public NutzJsonMessageConverter() {
+		  super(new MediaType[] { MediaType.APPLICATION_JSON, new MediaType("application", "*+json") });
+		 }
 
-	/**
-	 * 
-	 */
-	public NutzJsonMessageConverter() {
-		super(new MediaType[] { MediaType.APPLICATION_JSON, new MediaType("application", "*+json") });
+
+	@Override
+	public Object read(Type type, Class<?> contextClass, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+		return Json.fromJson(type, new InputStreamReader(inputMessage.getBody()));
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.springframework.http.converter.AbstractHttpMessageConverter#supports(
-	 * java.lang.Class)
-	 */
 	@Override
-	protected boolean supports(Class<?> paramClass) {
+	protected void writeInternal(Object t, Type type, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+		Json.toJson(new OutputStreamWriter(outputMessage.getBody()), t, format);
+	}
+
+	@Override
+	protected boolean supports(Class<?> clazz) {
 		return true;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.springframework.http.converter.AbstractHttpMessageConverter#
-	 * readInternal(java.lang.Class, org.springframework.http.HttpInputMessage)
-	 */
 	@Override
-	protected Object readInternal(Class<? extends Object> clazz, HttpInputMessage inputMessage)
-			throws IOException, HttpMessageNotReadableException {
-		Reader reader = new InputStreamReader(inputMessage.getBody(), getCharset(inputMessage.getHeaders()));
-		try {
-			return Json.fromJson(clazz, reader);
-		} catch (Exception ex) {
-			throw new HttpMessageNotReadableException("Could not read JSON: " + ex.getMessage(), ex);
-		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.springframework.http.converter.AbstractHttpMessageConverter#canWrite(java
-	 * .lang.Class, org.springframework.http.MediaType)
-	 */
-	@Override
-	public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+	public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
 		/**
 		 * 放过swagger
 		 */
@@ -112,34 +85,8 @@ public class NutzJsonMessageConverter extends AbstractHttpMessageConverter<Objec
 		return ignoreType == null || !ignoreType.matcher(clazz.getName()).matches();
 	}
 
-	/**
-	 * @param headers
-	 * @return
-	 */
-	private Charset getCharset(HttpHeaders headers) {
-		if ((headers == null) || (headers.getContentType() == null)
-				|| (headers.getContentType().getCharset() == null)) {
-			return DEFAULT_CHARSET;
-		}
-		return headers.getContentType().getCharset();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.springframework.http.converter.AbstractHttpMessageConverter#
-	 * writeInternal(java.lang.Object, org.springframework.http.HttpOutputMessage)
-	 */
 	@Override
-	protected void writeInternal(Object obj, HttpOutputMessage outputMessage)
-			throws IOException, HttpMessageNotWritableException {
-		Charset charset = getCharset(outputMessage.getHeaders());
-		OutputStreamWriter writer = new OutputStreamWriter(outputMessage.getBody(), charset);
-		try {
-			Json.toJson(writer, obj, format);
-		} catch (Exception ex) {
-			throw new HttpMessageNotWritableException("Could not write JSON: " + ex.getMessage(), ex);
-		}
+	protected Object readInternal(Class<? extends Object> clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+		return Json.fromJson(clazz, new InputStreamReader(inputMessage.getBody()));
 	}
-
 }
