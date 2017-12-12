5b29b8de6fde1718e5b991e9220aa14c724395b8
Wendal Chen <wendal1985@gmail.com>
2017-12-12 19:40:11 +0800

    add: 添加一个SpringIocLoader2,有选择地代理Spring的ioc对象

---
+++ nutz-integration-spring/src/main/java/org/nutz/integration/spring/SpringIocLoader2.java
@@ -0,0 +1,37 @@
+package org.nutz.integration.spring;
+
+import java.util.Arrays;
+
+import org.nutz.ioc.IocLoader;
+import org.nutz.ioc.IocLoading;
+import org.nutz.ioc.Iocs;
+import org.nutz.ioc.ObjectLoadException;
+import org.nutz.ioc.meta.IocObject;
+import org.springframework.context.ApplicationContext;
+
+public class SpringIocLoader2 implements IocLoader {
+
+    protected ApplicationContext applicationContext;
+
+    protected String[] names;
+
+    public SpringIocLoader2(ApplicationContext applicationContext, String[] names) {
+        this.applicationContext = applicationContext;
+        this.names = names;
+    }
+
+    public String[] getName() {
+        return names;
+    }
+
+    public IocObject load(IocLoading loading, String name) throws ObjectLoadException {
+        if (!has(name))
+            throw new ObjectLoadException("Object '" + name + "' without define!");
+        return Iocs.wrap(applicationContext.getBean(name));
+    }
+
+    public boolean has(String name) {
+        return Arrays.binarySearch(names, name) > -1;
+    }
+
+}
