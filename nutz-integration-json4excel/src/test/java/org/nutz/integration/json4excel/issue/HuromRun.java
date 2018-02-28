579250d71cd26e93ba7aacb3603e1126cfa7fb6a
pangwu86 <pangwu86@gmail.com>
2018-02-28 13:38:40 +0800

    update:调整下测试用例

--- nutz-integration-json4excel/src/test/java/org/nutz/integration/json4excel/issue/HuromRun.java
+++ nutz-integration-json4excel/src/test/java/org/nutz/integration/json4excel/issue/HuromRun.java
@@ -1,5 +1,7 @@
 package org.nutz.integration.json4excel.issue;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.InputStream;
 import java.util.List;
 
@@ -7,15 +9,13 @@ import org.junit.Test;
 import org.nutz.integration.json4excel.J4E;
 import org.nutz.integration.json4excel.TestUtil;
 import org.nutz.integration.json4excel.bean.HuromProduct;
-import org.nutz.json.Json;
-import org.nutz.json.JsonFormat;
 
 public class HuromRun extends TestUtil {
 
     @Test
     public void loadExcel() throws Exception {
-        InputStream in = this.getClass().getResourceAsStream("原汁机规格参数.xlsx");
+        InputStream in = this.getClass().getResourceAsStream("hdata.xlsx");
         List<HuromProduct> ps = J4E.fromExcel(in, HuromProduct.class, null);
-        System.out.println(Json.toJson(ps, JsonFormat.full()));
+        assertEquals(2, ps.size());
     }
 }
