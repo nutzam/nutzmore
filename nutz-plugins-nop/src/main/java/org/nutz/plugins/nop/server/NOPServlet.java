c9d285001dd34c3244fddfe4d7581236a15b73aa
kerbores <kerbores@zhcs.club>
2018-03-30 20:58:10 +0800

    add:必要的调试日志

--- nutz-plugins-nop/src/main/java/org/nutz/plugins/nop/server/NOPServlet.java
+++ nutz-plugins-nop/src/main/java/org/nutz/plugins/nop/server/NOPServlet.java
@@ -2,6 +2,7 @@ package org.nutz.plugins.nop.server;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
@@ -12,6 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.nutz.ioc.impl.PropertiesProxy;
 import org.nutz.json.Json;
+import org.nutz.lang.ContinueLoop;
+import org.nutz.lang.Each;
+import org.nutz.lang.ExitLoop;
+import org.nutz.lang.Lang;
+import org.nutz.lang.LoopException;
 import org.nutz.lang.Strings;
 import org.nutz.lang.Times;
 import org.nutz.log.Log;
@@ -42,12 +48,11 @@ public class NOPServlet extends HttpServlet {
 	private String configPath;
 
 	private String digestName = "MD5";
-	
-	private String fetcherName ="default";
 
+	private String fetcherName = "default";
 
 	Log log = Logs.get();
-	
+
 	/**
 	 * 默认5秒超时,如果实现了对时,那么5秒足以,如果不对时请酌情调整此时间
 	 */
@@ -63,8 +68,8 @@ public class NOPServlet extends HttpServlet {
 		PropertiesProxy proxy = new PropertiesProxy(configPath);
 		timeout = proxy.getInt("timeout", timeout);
 	}
-	
-	public  Date addSeconds(Date base, long seconds) {
+
+	public Date addSeconds(Date base, long seconds) {
 		return Times.D(base.getTime() + seconds * 1000);
 	}
 
@@ -76,13 +81,19 @@ public class NOPServlet extends HttpServlet {
 	 * , javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(final HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		/**
-		 * 1. 时间戳校验
-		 * 2.签名校验转移到nutz的filter中去处理,这样便于获取ioc中的对象
-		 * 3.方法校验
+		 * 1. 时间戳校验 2.签名校验转移到nutz的filter中去处理,这样便于获取ioc中的对象 3.方法校验
 		 * 
 		 */
+		Enumeration<String> headerNames = request.getHeaderNames();
+		Lang.each(headerNames, new Each<String>() {
+
+			@Override
+			public void invoke(int index, String key, int length) throws ExitLoop, ContinueLoop, LoopException {
+				log.debugf("header name : %s and header value is : %s", key, request.getHeader(key)); // 先全部打出来
+			}
+		});
 		try {
 			String method = request.getHeader(NOPConfig.methodKey);
 			if (Strings.isBlank(method)) {// 空方法
@@ -94,12 +105,12 @@ public class NOPServlet extends HttpServlet {
 				response.getWriter().write(Json.toJson(NOPData.exception("no timeStemp")));
 				return;
 			}
-			long  time = Long.parseLong(timeStemp);
+			long time = Long.parseLong(timeStemp);
 			if (addSeconds(Times.D(time), timeout).before(Times.now())) {
 				response.getWriter().write(Json.toJson(NOPData.exception("request timeout")));
 				return;
 			}
-//			转发之前把签名相关的信息带过去
+			// 转发之前把签名相关的信息带过去
 			request.setAttribute("digestName", digestName);
 			request.setAttribute("fetcherName", fetcherName);
 			request.getRequestDispatcher(method).forward(request, response);// 将请求转发给真实的函数入口
