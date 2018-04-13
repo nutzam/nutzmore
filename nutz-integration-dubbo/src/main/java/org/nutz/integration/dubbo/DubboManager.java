c71e417c3065e58f81d5091459c090849ee88335
Wendal Chen <wendal1985@gmail.com>
2018-04-13 09:58:24 -0700

    add: 支持非dubbo.xml的方式进行配置，走conf对象

--- nutz-integration-dubbo/src/main/java/org/nutz/integration/dubbo/DubboManager.java
+++ nutz-integration-dubbo/src/main/java/org/nutz/integration/dubbo/DubboManager.java
@@ -6,29 +6,46 @@ import java.util.Map.Entry;
 
 import org.nutz.ioc.Ioc;
 import org.nutz.ioc.meta.IocObject;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
 
 import com.alibaba.dubbo.config.ProtocolConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.ServiceConfig;
 
 public class DubboManager {
 
-    protected Ioc ioc;
-    
-    protected Map<String, IocObject> iobjs;
-    
-    public void init() {
-    	for (Entry<String, IocObject> en : new HashSet<>(iobjs.entrySet())) {
-    		if (en.getValue().getType().isAssignableFrom(AnnotationBean.class)) {
-            	ioc.get(AnnotationBean.class, en.getKey());
-            }
-    	}
-        for (Entry<String, IocObject> en : iobjs.entrySet()) {
-            if (ServiceConfig.class.isAssignableFrom(en.getValue().getType()))
-                ioc.get(ServiceConfig.class, en.getKey());
-        }
-    }
-    
-    public void depose() {
-        ProtocolConfig.destroyAll();
-    }
+	private static final Log log = Logs.get();
+
+	protected Ioc ioc;
+
+	protected Map<String, IocObject> iobjs;
+
+	protected String beanName;
+
+	public void _init() {
+		this.init();
+	}
+
+	public void init() {
+		log.debug("dubbo obj count=" + iobjs.size());
+		for (Entry<String, IocObject> en : new HashSet<>(iobjs.entrySet())) {
+			if (en.getValue().getType().isAssignableFrom(AnnotationBean.class)) {
+				ioc.get(AnnotationBean.class, en.getKey());
+			}
+		}
+		for (Entry<String, IocObject> en : iobjs.entrySet()) {
+			if (ServiceConfig.class.isAssignableFrom(en.getValue().getType()))
+				ioc.get(ServiceConfig.class, en.getKey());
+		}
+	}
+
+	public void depose() {
+		ProtocolConfig.destroyAll();
+		RegistryConfig.destroyAll();
+	}
+
+	public void setIoc(Ioc ioc) {
+		this.ioc = ioc;
+	}
 }
