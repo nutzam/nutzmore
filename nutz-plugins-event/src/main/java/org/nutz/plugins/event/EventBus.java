c59872005bf1151ca5ce8567c5a02ade4ff9d15e
zhengenshen <zhengenshen@gmail.com>
2019-07-04 13:46:20 +0800

    update:event插件改造使用泛型接口。

--- nutz-plugins-event/src/main/java/org/nutz/plugins/event/EventBus.java
+++ nutz-plugins-event/src/main/java/org/nutz/plugins/event/EventBus.java
@@ -2,26 +2,27 @@ package org.nutz.plugins.event;
 
 /**
  * 事件中心接口
- * @see org.nutz.plugins.event.impl.JvmEventBus
- * @see org.nutz.plugins.event.impl.RedisEventBus
- * 
+ *
  * @author qinerg@gmail.com
  * @varsion 2017-5-16
+ * @see org.nutz.plugins.event.impl.JvmEventBus
+ * @see org.nutz.plugins.event.impl.RedisEventBus
  */
 public interface EventBus {
-	/**
-	 * 初始化注册事件
-	 */
-	void init();
+    /**
+     * 初始化注册事件
+     */
+    void init();
 
-	/**
-	 * 派发事件
-	 * @param event
-	 */
-	void fireEvent(Event event);
+    /**
+     * 派发事件
+     *
+     * @param event event
+     */
+    <T extends Event> void fireEvent(T event);
 
-	/**
-	 * 销毁操作
-	 */
-	void depose();
+    /**
+     * 销毁操作
+     */
+    void depose();
 }
