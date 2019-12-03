e12f9619b55578503c2e979904a77d3a2deb6b63
kerbores <wangguiyuan@chinarecrm.com.cn>
2019-12-03 20:03:48 +0800

    add: 先简单支持一下sqltpl

--- nutz-plugins-spring-boot-starter/src/main/java/org/nutz/plugin/spring/boot/NutzDaoAutoConfiguration.java
+++ nutz-plugins-spring-boot-starter/src/main/java/org/nutz/plugin/spring/boot/NutzDaoAutoConfiguration.java
@@ -1,35 +1,63 @@
 package org.nutz.plugin.spring.boot;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.nutz.dao.Dao;
 import org.nutz.dao.SqlManager;
+import org.nutz.dao.Sqls;
 import org.nutz.dao.impl.DaoRunner;
 import org.nutz.dao.impl.NutDao;
 import org.nutz.integration.spring.SpringDaoRunner;
+import org.nutz.plugin.spring.boot.config.SqlTemplateProperties;
+import org.nutz.plugins.sqltpl.impl.beetl.BeetlSqlTpl;
+import org.nutz.plugins.sqltpl.impl.freemarker.FreeMarkerSqlTpl;
+import org.nutz.plugins.sqltpl.impl.jetbrick.JetbrickSqlTpl;
+import org.nutz.plugins.sqltpl.impl.velocity.VelocitySqlTpl;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnClass({Dao.class})
-@AutoConfigureAfter({DataSourceAutoConfiguration.class, SqlManagerAutoConfiguration.class})
+@ConditionalOnClass({ Dao.class })
+@AutoConfigureAfter({ DataSourceAutoConfiguration.class, SqlManagerAutoConfiguration.class })
+@EnableConfigurationProperties(SqlTemplateProperties.class)
 public class NutzDaoAutoConfiguration {
 
-    @Bean
-    public Dao dao(DataSource dataSource, SqlManager sqlManager, DaoRunner daoRunner) {
-        NutDao dao = new NutDao(dataSource, sqlManager);
-        dao.setRunner(daoRunner);
-        return dao;
-    }
+	@Bean
+	public Dao dao(DataSource dataSource, SqlManager sqlManager, DaoRunner daoRunner) {
+		NutDao dao = new NutDao(dataSource, sqlManager);
+		dao.setRunner(daoRunner);
+		return dao;
+	}
 
-    @Bean
-    @ConditionalOnMissingBean(DaoRunner.class)
-    public DaoRunner daoRunner() {
-        return new SpringDaoRunner();
-    }
+	@Bean
+	@ConditionalOnMissingBean(DaoRunner.class)
+	public DaoRunner daoRunner() {
+		return new SpringDaoRunner();
+	}
+
+	@PostConstruct
+	public void initSqlTemplate(SqlTemplateProperties sqlTemplateProperties) {
+		if (sqlTemplateProperties.isEnable()) {
+			switch (sqlTemplateProperties.getType()) {
+			case BEETL:
+				Sqls.setSqlBorning(BeetlSqlTpl.class);
+				break;
+			case FREEMARKER:
+				Sqls.setSqlBorning(FreeMarkerSqlTpl.class);
+				break;
+			case JETBRICK:
+				Sqls.setSqlBorning(JetbrickSqlTpl.class);
+			default:
+				Sqls.setSqlBorning(VelocitySqlTpl.class);
+				break;
+			}
+		}
+	}
 
 }
---
+++ nutz-plugins-spring-boot-starter/src/main/java/org/nutz/plugin/spring/boot/config/SqlTemplateProperties.java
@@ -0,0 +1,48 @@
+package org.nutz.plugin.spring.boot.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "nutz.dao.sqltpl")
+public class SqlTemplateProperties {
+
+	public static enum Type {
+		BEETL, FREEMARKER,JETBRICK,VELOCITY
+	}
+
+	Type type;
+	
+	boolean enable = false;
+
+	
+	
+	/**
+	 * @return the enable
+	 */
+	public boolean isEnable() {
+		return enable;
+	}
+
+	/**
+	 * @param enable the enable to set
+	 */
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public Type getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(Type type) {
+		this.type = type;
+	}
+	
+	
+
+}
